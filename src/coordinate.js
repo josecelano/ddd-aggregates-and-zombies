902853a315b7111bc17958ded1ace0ae98a46fe0
Jose Celano <jose.celano@hyve.net>
2021-05-17 23:27:33 +0100

    guard that world coordinate is zero or positive integer

--- src/coordinate.js
+++ src/coordinate.js
@@ -1,5 +1,17 @@
 class Coordinate {
   constructor(row, column) {
+    if (!Number.isInteger(row) || row < 0) {
+      throw new RangeError(
+        `The coordinate row must be a positive integer.${row}`
+      );
+    }
+
+    if (!Number.isInteger(column) || column < 0) {
+      throw new RangeError(
+        `The coordinate column must be a positive integer.${column}`
+      );
+    }
+
     this.rowNumber = row;
     this.columNumber = column;
   }
--- tests/coordinate.test.js
+++ tests/coordinate.test.js
@@ -6,3 +6,45 @@ it("should represent a row and column position in the world", () => {
   expect(coordinate.row()).toBe(1);
   expect(coordinate.column()).toBe(2);
 });
+
+it("should contain only zero or positive values for rows", () => {
+  const invalidValues = [
+    [null, 1],
+    ["", 1],
+    [" ", 1],
+    [-1, 1],
+    [1.2, 1],
+    [-1.2, 1],
+  ];
+
+  invalidValues.forEach(function (value) {
+    const t = (value) => {
+      new Coordinate(value[0], value[1]);
+    };
+
+    expect(() => {
+      t(value);
+    }).toThrow(RangeError);
+  });
+});
+
+it("should contain only zero or positive values for colums", () => {
+  const invalidValues = [
+    [1, null],
+    [1, ""],
+    [1, " "],
+    [1, -1],
+    [1, 1.2],
+    [1, -1.2],
+  ];
+
+  invalidValues.forEach(function (value) {
+    const t = (value) => {
+      new Coordinate(value[0], value[1]);
+    };
+
+    expect(() => {
+      t(value);
+    }).toThrow(RangeError);
+  });
+});
