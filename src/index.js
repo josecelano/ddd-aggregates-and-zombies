061ef03c2376ffecd0ec5f81da36ff630f3683c4
Jose Celano <jose.celano@hyve.net>
2021-05-21 21:07:13 +0100

    use independent independent instances for zombies and add them dinamically until the world is full of zombies

--- src/index.js
+++ src/index.js
@@ -3,24 +3,76 @@ import Coordinate from "./coordinate";
 import print_world from "./print_world";
 import Zombie from "./zombie";
 
+// World size
 const rows = 10;
 const columns = 10;
 
 const world = new ApocalypticWorld(rows, columns);
-const zombie = new Zombie();
+const zombies = [];
 
-function populate_world_zombies(world, zombie) {
+function populate_world_zombies(world, zombies) {
   for (let i = 0; i < world.numRows(); i++) {
     for (let j = 0; j < world.numColumns(); j++) {
-      const addZombie = Math.random() < 0.1; // 10% of cells with zombie
+      const addZombie = Math.random() < 0.1; // ~10% of cells with zombies
       if (addZombie) {
+        const zombie = new Zombie();
+        zombies.push(zombie);
         world.addZombie(zombie, new Coordinate(i, j));
       }
     }
   }
 }
 
-populate_world_zombies(world, zombie);
+function add_zombie_in_an_empty_cell(world, zombies) {
+  for (let i = 0; i < world.numRows(); i++) {
+    for (let j = 0; j < world.numColumns(); j++) {
+      const coordinate = new Coordinate(i, j);
+      if (!world.cellConstainsZombie(coordinate)) {
+        const zombie = new Zombie();
+        zombies.push(zombie);
+        world.addZombie(zombie, coordinate);
+        return;
+      }
+    }
+  }
+}
 
-/* eslint-disable no-console */
-console.log(print_world(world));
+function render_world(world, zombies) {
+  /* eslint-disable no-console */
+  console.clear();
+
+  /* eslint-disable no-console */
+  console.log(print_world(world));
+
+  /* eslint-disable no-console */
+  console.log(`Zombies: ${zombies.length}`);
+
+  /* eslint-disable no-console */
+  console.log('Ctrl-c to exit');    
+}
+
+function createInterval(f, interval, world, zombies) {
+  setInterval(function () {
+    f(world, zombies);
+  }, 1000);
+}
+
+populate_world_zombies(world, zombies);
+createInterval(add_zombie_in_an_empty_cell, 1000, world, zombies);
+createInterval(render_world, 500, world, zombies);
+
+const { stdin } = process;
+
+stdin.setRawMode(true);
+stdin.setEncoding("utf8");
+// Begin reading from stdin so the process does not exit.
+stdin.resume();
+
+stdin.on("data", function (key) {
+  // ctrl-c to exit
+  if (key === "\u0003") {
+    process.exit();
+  }
+  // write the key to stdout
+  process.stdout.write(key);
+});
