1d7f4976631f96caaa6848e09533eb1d9b1e78f9
Jose Celano <jose.celano@hyve.net>
2021-05-29 13:51:28 +0100

    add test for zombie

--- tests/zombie.test.js
+++ tests/zombie.test.js
@@ -41,3 +41,38 @@ it("should not move to another cells occupied by another zombie", () => {
 
   expect(zombie.moveToTheRight(world).equalsTo(coordinate)).toBe(true);
 });
+
+describe("random movement", () => {
+  it("should move randomly to an adjacent empty cell", () => {
+    const world = new ApocalypticWorld(1, 2);
+
+    // (0, 0) (0, 1)
+
+    const coordinate = new Coordinate(0, 0);
+    const zombie = new Zombie(coordinate);
+
+    world.markCellAsOccupiedByAZombie(coordinate);
+
+    const newCoordiante = zombie.moveRandomly(world);
+
+    expect(newCoordiante.equalsTo(new Coordinate(0, 1))).toBe(true);
+  });
+
+  it("should staty at the same cell if it cant move to any cell", () => {
+    const world = new ApocalypticWorld(1, 2);
+
+    // (0, 0) (0, 1)
+
+    const coordinate1 = new Coordinate(0, 0);
+    const coordinate2 = new Coordinate(0, 1);
+
+    const zombie1 = new Zombie(coordinate1);
+
+    world.markCellAsOccupiedByAZombie(coordinate1);
+    world.markCellAsOccupiedByAZombie(coordinate2);
+
+    const newCoordiante = zombie1.moveRandomly(world);
+
+    expect(newCoordiante.equalsTo(coordinate1)).toBe(true);
+  });
+});
