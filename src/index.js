bda0d5edaee1dd25edee78a5dd4333187d1f7173
Jose Celano <jose.celano@hyve.net>
2021-05-29 13:28:34 +0100

    refactor: extact const

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Game from "./game";
 
 // World configuration
+const percentageOfCellsOccupiedByZombies = 10;
 const rows = 45;
 const columns = 30;
-const initialNumberOfZombies = Math.floor((rows * columns) / 10); // 10% of cells
+const initialNumberOfZombies = Math.floor(
+  (rows * columns) / percentageOfCellsOccupiedByZombies
+);
 
 const game = new Game(rows, columns, initialNumberOfZombies);
 
