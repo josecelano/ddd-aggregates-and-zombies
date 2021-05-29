05fc3fefb92a5c136bf7ee7042a53696149321be
Jose Celano <jose.celano@hyve.net>
2021-05-29 14:44:14 +0100

    add test for zombies moving around

--- tests/game.test.js
+++ tests/game.test.js
@@ -56,3 +56,60 @@ it("should render the game in text format", () => {
 
   expect(output).toBe(" .. \nZombies: 0\nCtrl-c to exit");
 });
+
+it("should move zombies around", () => {
+  /*
+    Before:
+    (0,0)    (0,1)
+    OCCUPIED EMPTY
+
+    After:
+    (0,0)    (0,1)
+    EMPTY    OCCUPIED
+  */
+
+  const rows = 1;
+  const columns = 2;
+  const initialNumberOfZombies = 1;
+  const gameMenu = "\nZombies: 1\nCtrl-c to exit";
+
+  const game = new Game(rows, columns, initialNumberOfZombies);
+
+  game.populateWorldWithZombiesInRandomPositions();
+  game.moveZombies();
+
+  const output = game.render();
+  let finalOutputValid = false;
+
+  if (output == ` ..  🧟 ${gameMenu}` || output == ` 🧟  .. ${gameMenu}`) {
+    finalOutputValid = true;
+  }
+
+  expect(finalOutputValid).toBe(true);
+});
+
+it("should not move zombies where they can not go anywhere", () => {
+  /*
+    Before:
+    (0,0)    (0,1)
+    OCCUPIED OCCUPIED
+
+    After:
+    (0,0)    (0,1)
+    OCCUPIED OCCUPIED
+  */
+
+  const rows = 1;
+  const columns = 2;
+  const initialNumberOfZombies = 2;
+  const gameMenu = "\nZombies: 2\nCtrl-c to exit";
+
+  const game = new Game(rows, columns, initialNumberOfZombies);
+
+  game.populateWorldWithZombiesInRandomPositions();
+  game.moveZombies();
+
+  const output = game.render();
+
+  expect(output).toBe(` 🧟  🧟 ${gameMenu}`);
+});
