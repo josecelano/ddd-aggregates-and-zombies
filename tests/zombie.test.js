61c0ab81ab7ab6587770d666d0838e206f689e07
Jose Celano <jose.celano@hyve.net>
2021-06-24 12:55:15 +0100

    add tests for zombie

--- tests/zombie.test.js
+++ tests/zombie.test.js
@@ -9,11 +9,38 @@ it("should have a initial coordinate", () => {
   expect(zombie.getCoordinate()).toBe(coordinate);
 });
 
-/* TODO
-it("should not move outside the world", () => {
+it("should keep its current location if it can't move anywhere", () => {
+  const oneCellWorld = new ApocalypticWorld(1, 1);
+  const initialCoordinate = new Coordinate(0, 0);
+  const zombie = new Zombie(initialCoordinate);
 
-});*/
+  zombie.thinkWhereToWalk(oneCellWorld);
 
-/* TODO
-it("should not move to another cells occupied by another zombie", () => {
-});*/
+  expect(zombie.getNextCoordinate().equalsTo(initialCoordinate)).toBe(true);
+});
+
+it("should chose a random adjacent free cell to move on", () => {
+  const twoCellWorld = new ApocalypticWorld(1, 2);
+  const initialCoordinate = new Coordinate(0, 0);
+  const zombie = new Zombie(initialCoordinate);
+
+  // The only free coordinate in the world
+  const nextCoordinate = new Coordinate(0, 1);
+
+  zombie.thinkWhereToWalk(twoCellWorld);
+
+  expect(zombie.getNextCoordinate().equalsTo(nextCoordinate)).toBe(true);
+});
+
+it("should walk to the cell which was previously chosen to move on", () => {
+  const twoCellWorld = new ApocalypticWorld(1, 2);
+  const initialCoordinate = new Coordinate(0, 0);
+  const zombie = new Zombie(initialCoordinate);
+
+  zombie.thinkWhereToWalk(twoCellWorld);
+  const finalCoordinate = zombie.getNextCoordinate();
+
+  zombie.walk(twoCellWorld);
+
+  expect(zombie.getNextCoordinate().equalsTo(finalCoordinate)).toBe(true);
+});
